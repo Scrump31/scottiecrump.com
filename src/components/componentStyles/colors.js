ab8f65936d2c618c5292abc38b38bb69088b0260
Scottie Crump <sncrump29@gmail.com>
2021-08-16 14:08:00 -0400

    chore: remove current site to start from scratch

--- src/components/componentStyles/colors.js
+++
@@ -1,9 +0,0 @@
-const colors = {
-  orange: '#FFA500',
-  white: '#fff',
-  boxShadow: '0 2px 5px rgba(0, 0, 0, 0.26)',
-  logoBG: '#557585',
-  gray: '#f5f6f7',
-  contentBG: '#78909c',
-};
-export const { orange, white, boxShadow, logoBG, gray, contentBG } = colors;
