e91e27ee469f642cadaa62fa80bf51f67404b810
Scottie Crump <sncrump29@gmail.com>
2021-08-17 17:09:26 -0400

    feat: create rtl book post
    - clean up unnecessary content
    - delete unnecessary files

---
+++ data/productsData.js
@@ -0,0 +1,15 @@
+const productsData = [
+  {
+    title: 'My new book! Simplify Testing with React Testing Library',
+    description: `React Testing Library (RTL) is a lightweight and easy-to-use tool
+     for testing the document object model (DOM) output of components. 
+     This book will show you how to use this modern, user-friendly tool 
+     to test React components. Using skills gained throughout the book, 
+     you will learn how to reduce the risk that your application will 
+     not work as expected in production.`,
+    imgSrc: '/static/images/rtlBookCoverSample.png',
+    href: '/blog/rtl-book',
+  },
+]
+
+export default productsData
