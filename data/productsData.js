24efb171c16df2429c792817fc2c3ef2e1ac13dd
Scottie Crump <sncrump29@gmail.com>
2022-08-09 10:24:01 -0400

    feat: :sparkles: add promo blog for CTAL-ATT course

--- data/productsData.js
+++ data/productsData.js
@@ -10,6 +10,18 @@ const productsData = [
     imgSrc: '/static/images/rtlBookCoverSample.png',
     href: '/blog/rtl-book',
   },
+  {
+    title: 'Become an ISTQB® Certified Tester Advanced Level Agile Technical Tester!',
+    description: `
+    If you want to improve your Agile testing knowledge and skills, this course is for you!
+    The lessons are based on my years of real-life experience as an Agile tester. 
+    In addition, you'll learn advanced Agile testing techniques that will help you succeed
+    in today's competitive market. Even if you don't take the certification exam, you will still
+    learn a lot from this course. It will help you become a better tester and increase your job prospects.
+    `,
+    imgSrc: '/static/images/udemy-agile-att.jpeg',
+    href: '/blog/istqb-agile-technical-tester',
+  },
 ]
 
 export default productsData
