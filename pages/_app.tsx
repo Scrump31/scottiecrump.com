0ac44ed48ed5980a0bc12de71b6634ca4708528f
Scottie Crump <sncrump29@gmail.com>
2024-08-31 18:16:53 -0400

    fix: restore dark mode toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // @ts-ignore
-    <ThemeProvider>
+    <ThemeProvider attribute="class">
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
