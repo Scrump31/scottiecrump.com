cd3909d83c053024e09c6e54f67940c9b7a48d25
Scottie Crump <sncrump29@gmail.com>
2024-09-02 16:30:07 -0400

    chore: update generate-rss component

--- lib/generate-rss.tsx
+++ lib/generate-rss.tsx
@@ -1,20 +1,34 @@
 import { escape } from '@/lib/utils/htmlEscaper'
 
 import siteMetadata from '@/data/siteMetadata'
+import { FrontMatterProps } from '@/types/blog'
 
-const generateRssItem = (post) => `
+/**
+ * Generates a single Really Simple Syndication (RSS) item for a blog post.
+ *
+ * @param post - The blog post data.
+ * @returns A string representing the RSS item.
+ */
+const generateRssItem = (post: FrontMatterProps): string => `
   <item>
     <guid>${siteMetadata.siteUrl}/blog/${post.slug}</guid>
     <title>${escape(post.title)}</title>
     <link>${siteMetadata.siteUrl}/blog/${post.slug}</link>
     ${post.summary && `<description>${escape(post.summary)}</description>`}
-    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    <pubDate>${new Date(post.date || '').toUTCString()}</pubDate>
     <author>${siteMetadata.email} (${siteMetadata.author})</author>
     ${post.tags && post.tags.map((t) => `<category>${t}</category>`).join('')}
   </item>
 `
 
-const generateRss = (posts, page = 'feed.xml') => `
+/**
+ * Generates a Really Simple Syndication RSS feed for a list of blog posts.
+ *
+ * @param posts - The list of blog post data.
+ * @param page - The name of the RSS page (default is 'feed.xml').
+ * @returns A string representing the RSS feed.
+ */
+const generateRss = (posts: FrontMatterProps[], page = 'feed.xml'): string => `
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title>${escape(siteMetadata.title)}</title>
@@ -23,7 +37,7 @@ const generateRss = (posts, page = 'feed.xml') => `
       <language>${siteMetadata.language}</language>
       <managingEditor>${siteMetadata.email} (${siteMetadata.author})</managingEditor>
       <webMaster>${siteMetadata.email} (${siteMetadata.author})</webMaster>
-      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+      <lastBuildDate>${new Date(posts[0]?.date || '').toUTCString()}</lastBuildDate>
       <atom:link href="${siteMetadata.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
       ${posts.map(generateRssItem).join('')}
     </channel>
