710329a491881529bc0e1133969c9184b8c71e99
Scottie Crump <sncrump29@gmail.com>
2024-09-07 14:27:09 -0400

    Release 3.0.2 (#46)
    * Feat/SC-3-ts-pt-1 (#40)
    
    * chore: add typescript dependencies
    
    * chore: update analytics files
    
    * chore: update Products files
    
    * fix: next config
    
    * chore: update 404 page
    
    * chore: update about page
    
    * chore: update tags page
    
    * chore: update blog page
    
    * chore: update app page
    
    * fix: restore dark mode toggle
    
    * chore: index and document pages
    - also a types folder
    
    * chore: update docs
    
    * chore: update individual tag page
    
    * fix: resolve broken markdown code snippet layout
    
    * chore: update individual blog page
    
    * chore: update individual blog page part 2
    
    * chore: update individual blog page
    
    * chore: update header nav links
    
    * chore: update site meta data
    
    * fix: site meta data export
    
    * chore: update tailwind config
    
    * chore: update image component
    
    * chore: update page title component
    
    * chore: update section container component
    
    * chore: update kebabCase utility
    
    * chore: update Tag component
    
    * chore: update Pre component
    
    * chore: remove unused TOCInline component
    
    * chore: update ThemeSwitch component
    
    * chore: update formatDate
    
    * chore: update htmlEscaper
    
    * chore: update files util
    
    * chore: remove unused remark toc headings component
    
    * chore: remove unused remark components
    
    * chore: update generate-rss component
    
    * chore: update link component
    
    * chore: update post layout
    
    * feat: add reading time and total words
    
    * feat: release version 3
    - upgrade dependencies
    - upgrade to TypeScript
    
    * Feat/SC-8/ts-pt-2 (#42)
    
    * chore: update Author Layout
    
    * chore: update PostSimple
    
    * chore: update PostSimple to PostSimpleLayout
    
    * chore: update Pagination
    
    * chore: update LayoutWrapper
    
    * chore: update MobileNav
    
    * chore: update seo component
    
    * Feat/SC-6/vitest (#44)
    
    * test: initial vitest setup
    
    * chore: use global vitest imports
    
    * ci: add test pipeline (#45)
    
    * feat: release version 3.0.2

---
+++ tests/components/tag.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import Tag from '@/components/Tag'
+
+describe(`${Tag.name}`, () => {
+  test('when tag rendered with text, then returns kebab-case link', () => {
+    render(<Tag text="quality engineering" />)
+    const tagLink = screen.getByRole('link', {
+      name: /quality-engineering/i,
+    })
+    expect(tagLink).toBeInTheDocument()
+    expect(tagLink).toHaveAttribute('href', '/tags/quality-engineering')
+  })
+
+  test('when tag rendered without text, then returns empty string link', () => {
+    render(<Tag text="" />)
+    const tagLink = screen.getByRole('link')
+    expect(tagLink).toBeInTheDocument()
+    expect(tagLink).toHaveTextContent('')
+  })
+})
