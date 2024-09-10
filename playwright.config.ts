2eacd3401cb05671f5308a87e533412c75358694
Scottie Crump <sncrump29@gmail.com>
2024-09-09 21:41:53 -0400

    test: add spec to verify copy code snippet

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     video: 'retain-on-failure',
     trace: 'on-first-retry',
+    permissions: ['clipboard-read', 'clipboard-write'],
   },
 
   /* Configure projects for major browsers */
