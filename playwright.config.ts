f0fc4226f9be0b575602ccd36a69d91f8936dc7d
Scottie Crump <sncrump29@gmail.com>
2024-09-21 17:00:17 -0400

    Config InfluxDB (#80)
    * SC-15/influx db (#79)
    
    * chore: config influxDB
    
    * ci: add influxDB step
    
    * chore: add space
    
    * ci: add influxDB step to playwright job
    
    * ci: update script name
    
    * chore: update influxDB script

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['line'], ['allure-playwright']],
+  reporter: [
+    ['html'],
+    ['line'],
+    ['json', { outputFile: 'metrics_results/playwright-results.json' }],
+    ['allure-playwright'],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
