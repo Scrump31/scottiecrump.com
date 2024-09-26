85e60aa934e5d55f1c7d5908846a54b45f869e94
Scottie Crump <sncrump29@gmail.com>
2024-09-25 21:42:10 -0400

    feat: add datadog RUM (#90)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 
 import '@/css/tailwind.css'
+import { datadogRum } from '@datadog/browser-rum'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
@@ -9,6 +10,21 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
 export default function App({ Component, pageProps }: AppProps) {
+  datadogRum.init({
+    applicationId: process.env.NEXT_PUBLIC_DD_APPLICATION_ID ?? '',
+    clientToken: process.env.NEXT_PUBLIC_DD_CLIENT_TOKEN ?? '',
+    site: process.env.NEXT_PUBLIC_DD_SITE,
+    service: process.env.NEXT_PUBLIC_DD_SERVICE,
+    env: 'prod',
+    version: '3.0.2',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+  })
+
   return (
     // @ts-ignore
     <ThemeProvider attribute="class">
