b5033565468f2aaf9268b9ce1a6aa339497defa1
Scottie Crump <sncrump29@gmail.com>
2026-01-02 23:01:00 -0500

    chore: update products page test to reflect migration to app directory

--- tests/pages/products.test.tsx
+++ tests/pages/products.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Products from '@/pages/products'
+import Products from 'app/products/page'
 import ProductList from '@/data/productList'
 
 test('when render products page, then displays current products', () => {
